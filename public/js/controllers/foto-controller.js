3752cb8e58cec6ace78797dd3218b430d8cc1662
Paulo Eduardo Kopzinski Mello <eduardo.geleia@gmail.com>
2015-11-20 18:24:22 -0200

    trying to remove photo

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -20,4 +20,19 @@ angular.module('alurapic')
             }
         };
 
+	    $scope.remover = function(foto) {
+	    	console.loe("aaaa");
+
+	        $http.delete('/v1/fotos/' + foto._id)
+	        .success(function() {
+	            $scope.mensagem = 'Foto ' + foto.titulo + ' removida com sucesso!';
+
+	        })
+	        .error(function(erro) {
+	            console.log(erro);
+	            $scope.mensagem = 'Não foi possível apagar a foto ' + foto.titulo;
+	        });
+	    };
+
+
     });
