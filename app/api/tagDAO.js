af13613fb42325d3c2ffdd0cefbb8253818c00c2
Paulo Eduardo Kopzinski Mello <eduardo.geleia@gmail.com>
2016-03-27 00:20:01 -0300

    porta 80 e bons logs na TAG

--- app/api/tagDAO.js
+++ app/api/tagDAO.js
@@ -4,28 +4,45 @@ var tipos = require('./tipos');
 var tagDAO = {}
 
 tagDAO.adiciona = function(req, res) {
-    console.log('tagDAO.adiciona');
+    console.log('tagDAO.adiciona()');
+    console.log('Recebido(params)');
+    console.log(req.params);
+    console.log('Recebido(body)');
+    console.log(req.body);
+
 
     req.body.type = tipos.tag;
 
     db.insert(req.body, function(err, newDoc) {
         if(err) return console.log(err);
         console.log('Adicionado com sucesso: ' + newDoc._id);
+        console.log('Salvo:');
+        console.log(newDoc);
         res.json(newDoc._id);
     });  
 };
 
 tagDAO.busca = function(req, res) {
-    console.log('tagDAO.busca');
+    console.log('tagDAO.busca()');
+    console.log('Recebido(params):');
+    console.log(req.params);
+    console.log('Recebido(body):');
+    console.log(req.body);
    db.findOne({_id: req.params.tagId }, function(err, doc) {
         if (err) return console.log(err);
+       console.log('Retornado:');
+       console.log(doc);
         res.json(doc);
     });
 };
 
 tagDAO.atualiza = function(req, res) {
-    console.log('tagDAO.atualiza');
-    
+    console.log('tagDAO.atualiza()');
+    console.log('Recebido(params):');
+    console.log(req.params);
+    console.log('Recebido(body):');
+    console.log(req.body);
+
     db.update({_id : req.params.tagId }, req.body, function(err, numReplaced) {
         if (err) return console.log(err);
         if(numReplaced) res.status(200).end();
@@ -36,17 +53,26 @@ tagDAO.atualiza = function(req, res) {
 };
 
 tagDAO.lista = function(req, res) {
-    console.log('tagDAO.lista');
+    console.log('tagDAO.lista()');
+    console.log('Recebido(params)');
+    console.log(req.params);
+    console.log('Recebido(body)');
+    console.log(req.body);
 
     db.find({type : tipos.tag}).sort({texto: 1}).exec(function(err, doc) {
-        console.log(doc);
         if (err) return console.log(err);
         res.json(doc);
+        console.log('Retornado:');
+        console.log(doc);
     });
 };
 
 tagDAO.remove = function(req, res) {
-    console.log('tagDAO.remove');
+    console.log('tagDAO.remove()');
+    console.log('Recebido(params)');
+    console.log(req.params);
+    console.log('Recebido(body)');
+    console.log(req.body);
 
     db.remove({ _id: req.params.tagId }, {}, function (err, numRemoved) {
         if (err) return console.log(err);
